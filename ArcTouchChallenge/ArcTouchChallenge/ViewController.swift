d5e915e5f6cde0cc88ef03a78d73f3f7d43b5166
Arthur Augusto Sousa Marques <aasmarques93@gmail.com>
2018-03-13 16:00:54 -0300

    Creating project

---
+++ ArcTouchChallenge/ArcTouchChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArcTouchChallenge
+//
+//  Created by Arthur Augusto Sousa Marques on 3/13/18.
+//  Copyright © 2018 Arthur Augusto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
